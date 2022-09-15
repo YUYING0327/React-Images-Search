5c4964b61004438ac75afb3c3e959836fc97b8f4
YUYING0327 <105634108+YUYING0327@users.noreply.github.com>
2022-09-15 15:03:57 +0800

    Create Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
+import { createContext } from 'react';
 import Images from './components/Images';
 import Jumbutron from './components/Jumbutron';
 import SearchField from './components/SearchField';
 import useAxios from './hooks/useAxios';
 
+// Create Context
+export const ImageContext = createContext();
+
 function App() {
   const { response, isLoading, error, fetchData } = useAxios(
     `search/photos?page=1&query=office&client_id=I6dXYYNPlW4_U6UZ1fvQsgrW1oWpTEQRmM7-g4XeU5U`
   );
-  console.log(response);
+
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+  };
 
   return (
-    <>
+    <ImageContext.Provider value={value}>
       <Jumbutron>
         <SearchField />
       </Jumbutron>
       <Images />
-    </>
+    </ImageContext.Provider>
   );
 }
 
