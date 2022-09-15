02596dc777161aa74f87283c4339753abed2ac8d
YUYING0327 <105634108+YUYING0327@users.noreply.github.com>
2022-09-15 14:54:14 +0800

    create useAxios custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Images from './components/Images';
 import Jumbutron from './components/Jumbutron';
 import SearchField from './components/SearchField';
+import useAxios from './hooks/useAxios';
 
 function App() {
+  const { response, isLoading, error, fetchData } = useAxios(
+    `search/photos?page=1&query=office&client_id=I6dXYYNPlW4_U6UZ1fvQsgrW1oWpTEQRmM7-g4XeU5U`
+  );
+  console.log(response);
+
   return (
     <>
       <Jumbutron>
